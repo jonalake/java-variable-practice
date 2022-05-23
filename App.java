57fe97e8746dd4c7301c4a7287ee13e255dd3fcf
Jon Lake <jlake21@ford.com>
2022-05-23 15:23:06 -0600

    Add Solution

--- App.java
+++ App.java
@@ -1,6 +1,24 @@
 class App {
-  public static void main(String args[]){
-    String message = "Hello, world!";
-    System.out.println(message);
+  public static void main(String args[]) {
+    int vin = 4562155;
+    System.out.println(vin);
+
+    String vehicle = "Ford Mustang";
+    System.out.println(vehicle);
+
+    String color = "Red";
+    System.out.println(color);
+
+    boolean hasTowPackage = false;
+    System.out.println(hasTowPackage);
+
+    int odo = 12345;
+    System.out.println(odo);
+
+    double price = 50000.00;
+    System.out.println(price);
+
+    char quality = 'A';
+    System.out.println(quality);
   }
 }
